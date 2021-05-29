b16ccd3de43e01744f246e143544f2ed5513f843
SoKnight <mcliyanage2018@gmail.com>
2021-05-29 16:54:10 +0500

    Forgotten class uploaded

--- api-v2/src/main/java/ru/soknight/easydonate/sdk/v2/json/deserialize/DateDeserializer.java
+++ api-v2/src/main/java/ru/soknight/easydonate/sdk/v2/json/deserialize/DateDeserializer.java
@@ -1,27 +1,23 @@
 package ru.soknight.easydonate.sdk.v2.json.deserialize;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import ru.soknight.easydonate.sdk.v2.EasyDonateSDK;
+
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.TemporalAccessor;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-
-import ru.soknight.easydonate.sdk.v2.EasyDonateSDK;
-
 public class DateDeserializer extends JsonDeserializer<LocalDateTime> {
 
     @Override
-    public LocalDateTime deserialize(JsonParser parser, DeserializationContext context)
-            throws IOException, JsonProcessingException
-    {
+    public LocalDateTime deserialize(JsonParser parser, DeserializationContext context) throws IOException {
         String valueAsString = parser.getValueAsString();
         if(valueAsString == null) return null;
         
         TemporalAccessor accessor = EasyDonateSDK.DATE_FORMATTER.parse(valueAsString);
-        return accessor != null ? LocalDateTime.from(accessor) : null;
+        return LocalDateTime.from(accessor);
     }
 
 }
